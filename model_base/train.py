cdf67cd6de4a45afb9ce69b7590bd770465dbd2d
Daniela <1703695@uab.cat>
2026-05-27 00:36:52 +0200

    Optional parameters + Tf_ratio fins a 0

--- model_base/train.py
+++ model_base/train.py
@@ -103,17 +103,17 @@ def train(model, train_loader, val_loader, optimizer, criterion, config, device)
     wandb.watch(model, criterion, log="all", log_freq=50)
     best_val_loss = float('inf')
 
+    tf_ratio = 1.0
+
     for epoch in range(config.epochs):
         print(f"\n=== Epoch {epoch+1}/{config.epochs} ===")
 
         if config.teacher_forcing:
             # decaïment en escales:
-            if epoch < 10:
-                tf_ratio = 1.0
-            elif epoch < 20:       
-                tf_ratio = 0.85     
-            else:                   
-                tf_ratio = 0.70    
+            if epoch < 140:
+                tf_ratio = 1- np.floor(epoch/14)*0.1
+            else: 
+                tf_ratio=0   
         else:
             # Sense Teacher Forcing: el model sempre usa el seu propi token
             tf_ratio = 0.0  
